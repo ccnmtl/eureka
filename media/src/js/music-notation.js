22ae72cf544eb13716e300b9c30411751226d67d
Nick Buonincontri <nbuonin@gmail.com>
2020-06-24 16:18:52 -0400

    Adjust height to remove extra bottom margin

--- media/src/js/music-notation.js
+++ media/src/js/music-notation.js
@@ -1,33 +1,44 @@
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
 
 // Render music notation examples
-const BOTTOM_MARGIN_ADJUSTMENT = 140;
 let mContainers = Array.from(
     document.getElementsByClassName('music-container'));
 
 let osmdOptions = {
     drawingParameters: 'compacttight',
     drawPartNames: false,
+    pageFormat: 'endless',
+    autoResize: false,
+};
+
+const BOTTOM_MARGIN_ADJUSTMENT = 140;
+let setOsmdHeight = (svg, height) => {
+    // Fix the height of the examples, temp fix until
+    // OpenSheetMusicDisplay fixes this issue:
+    // https://github.com/opensheetmusicdisplay/opensheetmusicdisplay/issues/788
+
+    // Adjust the height
+    svg.setAttribute('height', height);
+
+    // Adjust the viewBox
+    let viewBox = svg.getAttribute('viewBox').split(' ');
+    viewBox[3] = String(height);
+    svg.setAttribute('viewBox', viewBox.join(' '));
 };
 
 mContainers.map((el, idx) => {
     el.id = 'music-container-' + idx;
     let osmd = new OpenSheetMusicDisplay(el, osmdOptions);
     osmd.load(el.dataset.musicxml)
-        .then(() => {osmd.render();})
         .then(() => {
-            // Fix the height of the examples, temp fix until
-            // OpenSheetMusicDisplay fixes this issue:
-            // https://github.com/opensheetmusicdisplay/opensheetmusicdisplay/issues/788
+            osmd.EngravingRules.PageBottomMargin = 0.0;
+            osmd.render();
+        })
+        .then(() => {
             let svg = el.getElementsByTagName('svg')[0];
-            // Adjust the height
             let height = (
                 Number(svg.getAttribute('height')) - BOTTOM_MARGIN_ADJUSTMENT);
-            svg.setAttribute('height', height);
-            // Adjust the viewBox
-            let viewBox = svg.getAttribute('viewBox').split(' ');
-            viewBox[3] = String(height);
-            svg.setAttribute('viewBox', viewBox.join(' '));
+            setOsmdHeight(svg, height);
         });
 });
 
