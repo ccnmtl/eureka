aa2cbff8e2dc393c5753d8d25b55f9d33ad2901d
Nick Buonincontri <nbuonin@gmail.com>
2020-07-21 23:26:02 -0400

    Update Cypress URLs

--- cypress/integration/axe/earTrainingLevel.spec.js
+++ cypress/integration/axe/earTrainingLevel.spec.js
@@ -1,5 +1,5 @@
 it('Tests the ear training level template', function() {
-    cy.visit('/ear-training/introduction-to-ear-training-one/');
+    cy.visit('/ear-training/ear-training-one/');
     cy.injectAxe();
     cy.checkA11y();
 });
